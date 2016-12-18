f7e05641cf5955eab31ba141e5b40a7b436b06ac
kantuni <henrikh.kantuni@gmail.com>
2016-12-18 00:02:33 -0500

    Add C++ version

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Geometry
+//
+//  Created by Henrikh Kantuni on 12/17/16.
+//  Copyright © 2016 Henrikh Kantuni. All rights reserved.
+//
+
+#include <iostream>
+#include "point.cpp"
+#include "line.cpp"
+
+int main(int argc, const char * argv[]) {
+  
+  return 0;
+}
